a43544904cdd77174fcfaa591a0d4fd3e270c27e
geotecc <aws@geotecconsultoria.com>
2025-07-20 14:34:47 -0300

    Add next.config.ts 1753032886994

---
+++ next.config.ts
@@ -0,0 +1,17 @@
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  output: 'standalone',
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        pathname: '/photos/**',
+      },
+    ],
+  },
+}
+
+export default nextConfig
